f7dec828553fb2781a71c71b9a01912069f729ec
chenwuxiong <chenwuxiong@daihoubang.com>
2017-11-30 14:36:37 +0800

    Initial commit

---
+++ src/main/java/com/wz/VisoApp/entity/Test.java
@@ -0,0 +1,17 @@
+package com.wz.VisoApp.entity;
+
+/**
+ * Created by chenwuxiong on 2017/11/30.
+ */
+public class Test {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
